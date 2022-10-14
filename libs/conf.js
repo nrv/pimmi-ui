7c168eab62e23fd830fd39b797ebac362a0df7e6
Nicolas Hervé <nherve@ina.fr>
2022-10-14 18:08:46 +0200

    test

--- libs/conf.js
+++ libs/conf.js
@@ -1,3 +1,10 @@
-var MEDIA_SERVER_URI = "../pimmi/demo_dataset/dataset1/";
+// var MEDIA_SERVER_URI = "../pimmi/demo_dataset/dataset1/";
+// var DATA_FILE = "../pimmi/index/dataset1.ivf1024.mining.clusters.json";
+
+// var MEDIA_SERVER_URI = "../20201109/montage_graph/dataset/separated/";
+// var DATA_FILE = "../20201109/montage_graph/mining.clusters.json";
+
+var MEDIA_SERVER_URI = "../dataset1/images/";
+var DATA_FILE = "../dataset1/index/dataset1.IVF1024,Flat.mining.clusters.json";
+
 var USE_MAGNIFIER = false;
-var DATA_FILE = "../pimmi/index/dataset1.ivf1024.mining.clusters.json";
